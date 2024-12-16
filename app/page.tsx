b10402dde5112aa276d9c29ae763d3e2481fe4bf
Ritabrata Patra <teamritabrata@gmail.com>
2024-12-16 14:30:09 +0530

    updated globals.css for reusee

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-3xl text-brand">
+    <div className="flex items-center justify-center h-screen bg-amber-100">
+      <h1 className="h1">
       StoreIt - You Store It Here
     </h1>
     </div>
