9491ef912255a06e67748c4e73d2b689e974f1b4
Ritabrata Patra <teamritabrata@gmail.com>
2024-12-16 14:33:43 +0530

    updated fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 
 const poppins = Poppins({
-  variable: "--font-poppins",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
 });
 
 
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${poppins.variable}`}
+        className={`${poppins.variable} font-poppins antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-amber-100">
+    <div className="flex-center h-screen">
       <h1 className="h1">
       StoreIt - You Store It Here
     </h1>
