367c157ecfff7ceff65b6ac6b0842f2b4667303b
Ritabrata Patra <teamritabrata@gmail.com>
2024-12-18 10:12:08 +0530

    implemented a loading state for performing file dropdown actions

--- components/ActionsDropDown.tsx
+++ components/ActionsDropDown.tsx
@@ -3,14 +3,13 @@ import { Models } from "node-appwrite";
 import React, { useState } from "react";
 
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-  } from "@/components/ui/dialog"
-  
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 import {
   DropdownMenu,
@@ -32,52 +31,64 @@ const ActionsDropDown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isDropDownOpen, setIsDropDownOpen] = useState(false);
   const [action, setAction] = useState<ActionType | null>(null);
-  const [name , setName] = useState(file.name)
-  const [isLoading , setIsLoading] = useState(false)
+  const [name, setName] = useState(file.name);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const closeAllModals = () => {
-        setIsModalOpen(false);
-        setIsDropDownOpen(false);
-        setAction(null);
-        setName(file.name)
-        //setEmails([]);
-    }
+  const closeAllModals = () => {
+    setIsModalOpen(false);
+    setIsDropDownOpen(false);
+    setAction(null);
+    setName(file.name);
+    //setEmails([]);
+  };
 
-    const handleAction = async () => {
-        setIsLoading(true)
-    }
+  const handleAction = async () => {
+    setIsLoading(true);
+  };
 
-    const renderDialogContent = () => {
-        if(!action) return null
-        const {value , label} = action ;
+  const renderDialogContent = () => {
+    if (!action) return null;
+    const { value, label } = action;
 
-      return (
-        <DialogContent className="shad-dialog-button">
-          <DialogHeader className="flex flex-col gap-3">
-            <DialogTitle className="text-center text-light-100">{label}</DialogTitle>
-            {value === "rename" && (
-              <DialogDescription className="text-center text-light-100">
-                <Input
-                  value = {name}
-                  type="text"
-                  placeholder={file.name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </DialogDescription>
-            )}
-          </DialogHeader>
-          {['rename' , 'share' , 'delete'].includes(value) && (
-            <DialogFooter className="flex flex-col gap-3 md:flex-row">
-                <Button variant="destructive" onClick={closeAllModals} className="modal-cancel-button border">Cancel</Button>
-                <Button onClick={handleAction} className="modal-submit-button">
-                    <p className="capitalize">{value}</p>
-                    {isLoading && <span><LoaderCircle className="animate-spin"/></span>}
-                </Button>
-            </DialogFooter>
+    return (
+      <DialogContent className="shad-dialog-button">
+        <DialogHeader className="flex flex-col gap-3">
+          <DialogTitle className="text-center text-light-100">
+            {label}
+          </DialogTitle>
+          {value === "rename" && (
+            <DialogDescription className="text-center text-light-100">
+              <Input
+                value={name}
+                type="text"
+                placeholder={file.name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </DialogDescription>
           )}
-        </DialogContent>
-      );
-    };
+        </DialogHeader>
+        {["rename", "share", "delete"].includes(value) && (
+          <DialogFooter className="flex flex-col gap-3 md:flex-row">
+            <Button
+              variant="destructive"
+              onClick={closeAllModals}
+              className="modal-cancel-button border"
+            >
+              Cancel
+            </Button>
+            <Button onClick={handleAction} className="modal-submit-button">
+              <p className="capitalize">{value}</p>
+              {isLoading && (
+                <span>
+                  <LoaderCircle className="animate-spin" />
+                </span>
+              )}
+            </Button>
+          </DialogFooter>
+        )}
+      </DialogContent>
+    );
+  };
   return (
     <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
       <DropdownMenu open={isDropDownOpen} onOpenChange={setIsDropDownOpen}>
