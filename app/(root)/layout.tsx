6d7eb7a8cb7e14414c785b30ca30ab6550049343
Ritabrata Patra <teamritabrata@gmail.com>
2024-12-18 16:21:27 +0530

    deployement tweaks related to dynamic routees

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,10 +5,10 @@ import { Toaster } from "@/components/ui/toaster";
 import { getCurrentUser } from "@/lib/actions/user.action";
 import { redirect } from "next/navigation";
 import React from "react";
-
+export const dynamic = "force-dynamic";
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
-
+  
   if (!currentUser) return redirect("/sign-in");
   return (
     <main className="flex h-screen">
